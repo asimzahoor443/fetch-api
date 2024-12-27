5009a2ee19d28c4e1b5e8064fd4e4ff5a10c1c43
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-12-27 10:25:08 +0500

    docs: updating script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log(fetch('https://jsonplaceholder.typicode.com/users'));
+fetch('https://jsonplaceholder.typicode.com/users/11')
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error('Not ok');
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
